2b1d92a3311e19ccdcd1d51379c1216cd04056a4
Tms Bodnar <tms.bodnar@gmail.com>
2017-10-26 15:51:56 +0200

    EarlyBeta         Bookings 60% done         Before new calendar view

--- app/src/main/java/okosnotesz/hu/okosnotesz/fragments/BookingsFragment.java
+++ app/src/main/java/okosnotesz/hu/okosnotesz/fragments/BookingsFragment.java
@@ -1,5 +1,6 @@
 package okosnotesz.hu.okosnotesz.fragments;
 
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -7,6 +8,8 @@ import android.content.pm.ResolveInfo;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.provider.CalendarContract.Events;
 import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -20,8 +23,10 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import okosnotesz.hu.okosnotesz.BookingActivity;
+import okosnotesz.hu.okosnotesz.CalendarActivity;
 import okosnotesz.hu.okosnotesz.R;
 import okosnotesz.hu.okosnotesz.model.Treatments;
 
@@ -34,7 +39,10 @@ public class BookingsFragment extends Fragment {
 
     final int PICK_CONTACT = 333;
     final int PICK_TREATMENT = 334;
+    final int PICK_DATE = 335;
     long startMillis;
+    String guestName = "";
+    int treatmentDuration = 0;
     TextView tvGuestName;
     TextView tvTreatmentsName;
     StringBuffer trenames = new StringBuffer("");
@@ -73,12 +81,35 @@ public class BookingsFragment extends Fragment {
                 startActivityForResult(intent, PICK_TREATMENT);
             }
         });
+        datePick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(context, CalendarActivity.class);
+                startActivity(i);
+               /*Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
+                builder.appendPath("time");
+                ContentUris.appendId(builder, startMillis);
+                Intent i = new Intent(Intent.ACTION_VIEW).setData(builder.build());
+                startActivityForResult(i, PICK_DATE );*/
+
+              /*  Intent intent = new Intent(Intent.ACTION_INSERT)
+                        .setData(Events.CONTENT_URI)
+                        .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, startMillis)
+                        .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, startMillis+treatmentDuration)
+                        .putExtra(Events.TITLE, trenames.toString())
+                        .putExtra(Events.DISPLAY_COLOR, )
+                        .putExtra(Events.DESCRIPTION, guestName);
+
+                startActivity(intent);*/
+
+            }
+        });
         return view;
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        String guestName = "";
+
 
         if(data != null && requestCode == PICK_CONTACT){
             Uri uri = data.getData();
@@ -103,11 +134,18 @@ public class BookingsFragment extends Fragment {
             trenames.delete(0, trenames.length());
             for (Treatments t : checkedList) {
                 trenames.append(t.getName()+ ", ");
-                Log.d("xxx", t.getName()+"!");
-                Log.d("xxx", trenames.toString());
+                treatmentDuration += TimeUnit.MINUTES.toMillis(t.getTime());
             }
         }
         tvTreatmentsName.setText(trenames.toString());
+
+        if(requestCode == PICK_DATE){
+            Log.d("xxx", "van válasz");
+            if(data == null) {
+                Log.d("xxx", "null");
+            }else
+                Log.d("xxx", "notnull");
+        }
 
     }
     public static boolean isIntentAvailable(Context context, String action)
